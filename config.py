b74ee70f0a43ab6588cf07913b2bf88ba0374a6d
harvey.donnelly <harveydonnelly404@gmail.com>
2021-12-11 20:34:07 -0500

    Changed absolute cases to cases per capita per 100,000.

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class TorontoConfig:
 
     def __init__(self) -> None:
 
-        self.start_date = datetime.date(2020, 3, 1)
-        self.end_date = datetime.date(2020, 4, 1)
+        self.start_date = datetime.date(2020, 12, 1)
+        self.end_date = datetime.date(2021, 3, 1)
 
         self.paths = {
             'regions': 'data/toronto_regions.csv',
