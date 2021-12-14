dfc3b288cb941bac6284256732600f1c3d081797
Ewan Jordan <ewan.jordan30@gmail.com>
2021-12-13 21:40:39 -0500

    updated config dates to be 09/2020 - 12/2021

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,12 @@
 """
-DOCSTRING
+Module Name: Configuration Module
+Source Path: modules/config.py
+
+Description:
+
+This module contains a class which important configuration variables, including file paths and starting date and
+ending date for covid case usage. It
+
 """
 
 import datetime
@@ -16,12 +23,13 @@ class TorontoConfig:
 
     def __init__(self) -> None:
 
-        self.start_date = datetime.date(2020, 12, 1)
-        self.end_date = datetime.date(2021, 3, 1)
+        self.start_date = datetime.date(2020, 9, 1)
+        self.end_date = datetime.date(2021, 12, 1)
 
         self.paths = {
-            'regions': 'data/toronto_regions.csv',
-            'cases': 'data/toronto_covid_cases.csv'
+            'regions': '../data/toronto_regions.csv',
+            'cases': '../data/toronto_covid_cases.csv',
+            'shapes': '../data/toronto_boundaries/Neighbourhoods.shp'
         }
         self.regression = {
             'angle_divisor': 1000
